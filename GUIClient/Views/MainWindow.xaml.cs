a3494d2c2f2a8e27bea6cdf62357df69b9959a46
Vadim Bondaruk <wadbondaruk@gmail.com>
2018-05-11 00:19:33 +0300

    Add time

--- GUIClient/Views/MainWindow.xaml.cs
+++ GUIClient/Views/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,10 +21,13 @@ namespace GUIClient.Views
 {
     public class MainWindow : Window
     {
-        private Button btnConsumerGet;
-        private TextBlock outputText;
+        private static Button btnConsumerGet;
+        private static TextBlock outputText;
+        private static TextBlock timeText;
+        private static Stopwatch sw = new Stopwatch();
         private static EncryptorKey clientEncryptorKey = new EncryptorKey();
         private static IDataEncryptor _dataEncryptor = new DataEncryptionLibrary.Helpers.DataEncryptor();
+        private static HubConnection hubConnection;
         public MainWindow()
         {
             InitializeComponent();
@@ -35,14 +39,43 @@ namespace GUIClient.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-            btnConsumerGet = this.Find<Button>("btnConsumerGet");
-            outputText = this.Find<TextBlock>("outputText");
-
+            btnConsumerGet = this.FindControl<Button>("btnConsumerGet");
+            outputText = this.FindControl<TextBlock>("outputText");
+            timeText = this.FindControl<TextBlock>("timeText");
+            ConfigureSignalR();
             btnConsumerGet.Click += BtnConsumerGet_Click;
-
+            
         }
 
         private async void BtnConsumerGet_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+
+            btnConsumerGet.IsEnabled = false;
+
+            sw.Start();
+            
+            await hubConnection.InvokeAsync("ConsumerMerchantsSeedRequestsGet", EncodeData(new { CompanyId = "37d82b6d-8e7a-437b-bd1f-c0d2b2b56e75" }));
+        }
+
+        public static string GetToken(string url)
+        {
+            var requestUri = url + "/api/auth";
+            var content = new StringContent("{\"id\":\"mobile_client\", \"secret\":\"secret\"}", Encoding.UTF8, "application/json");
+            HttpClient client = new HttpClient();
+            var data = JsonConvert.DeserializeObject(client.PostAsync(requestUri, content).Result.Content.ReadAsStringAsync().Result, typeof(TokenResponse));
+            return (data as TokenResponse)?.AccessToken;
+        }
+
+        public static string EncodeData(object data)
+        {
+            var stringData = JsonConvert.SerializeObject(data);
+
+            var encryptedData = _dataEncryptor.EncryptDataByAes256Gcm(stringData, clientEncryptorKey);
+
+            return System.Convert.ToBase64String(encryptedData);
+        }
+
+        public async void ConfigureSignalR()
         {
             // Client keys pair
             var client_x25519key = _dataEncryptor.CreateX25519EncryptorKey().Key;
@@ -70,7 +103,7 @@ namespace GUIClient.Views
 
             var factory = new HttpConnectionFactory(Options.Create(options), new LoggerFactory());
 
-            var hubConnection = new HubConnection(factory, new JsonHubProtocol(), null);
+            hubConnection = new HubConnection(factory, new JsonHubProtocol(), null);
 
             var methodsWithoutEncryption = new List<string>()
                 {
@@ -101,16 +134,20 @@ namespace GUIClient.Views
                     outputText.Text = JToken.Parse(data).ToString(Formatting.Indented);
                 });
             });
-
+            var window = this;
             methodsWithEncryption.ForEach(method =>
             {
-                hubConnection.On<String>(method, (data) =>
+                hubConnection.On<String>(method, async (data) =>
                 {
                     var bytes = System.Convert.FromBase64String(data);
                     data = _dataEncryptor.Decrypt(bytes, clientEncryptorKey);
-                    Dispatcher.UIThread.InvokeAsync(() => outputText.Text = JToken.Parse(data).ToString(Formatting.Indented));
-
-
+                    sw.Stop();
+                    await Dispatcher.UIThread.InvokeAsync(() => {
+                        outputText.Text = JToken.Parse(data).ToString(Formatting.Indented);
+                        timeText.Text = $"Time: {sw.ElapsedMilliseconds} ms";
+                        btnConsumerGet.IsEnabled = true;
+                        
+                    }, DispatcherPriority.Render);
                 });
             });
 
@@ -132,28 +169,6 @@ namespace GUIClient.Views
             await hubConnection.StartAsync();
 
             await hubConnection.InvokeAsync("PublicKeyExchange", System.Convert.ToBase64String(clientPublicKeyBytes));
-
-
-            await hubConnection.InvokeAsync("ConsumerMerchantsSeedRequestsGet", EncodeData(new { CompanyId = "37d82b6d-8e7a-437b-bd1f-c0d2b2b56e75" }));
-
-        }
-
-        public static string GetToken(string url)
-        {
-            var requestUri = url + "/api/auth";
-            var content = new StringContent("{\"id\":\"mobile_client\", \"secret\":\"secret\"}", Encoding.UTF8, "application/json");
-            HttpClient client = new HttpClient();
-            var data = JsonConvert.DeserializeObject(client.PostAsync(requestUri, content).Result.Content.ReadAsStringAsync().Result, typeof(TokenResponse));
-            return (data as TokenResponse)?.AccessToken;
-        }
-
-        public static string EncodeData(object data)
-        {
-            var stringData = JsonConvert.SerializeObject(data);
-
-            var encryptedData = _dataEncryptor.EncryptDataByAes256Gcm(stringData, clientEncryptorKey);
-
-            return System.Convert.ToBase64String(encryptedData);
         }
     }
 }
